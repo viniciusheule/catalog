f049e91026bf763c04c4ab6ec4e7905c59db9754
Vinicius Heule <viniciusheule@gmail.com>
2022-11-24 19:54:16 -0300

    POST new Category

--- backend/src/main/java/com/projeto/catalog/services/CategoryService.java
+++ backend/src/main/java/com/projeto/catalog/services/CategoryService.java
@@ -22,15 +22,23 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
-		
+
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
+
 	}
-	
+
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
 }
