59ca4faf3ddd698d6f01cd57bfb0eb40aaf5b75d
Vinicius Heule <viniciusheule@gmail.com>
2022-11-24 09:22:00 -0300

    DTO

--- backend/src/main/java/com/projeto/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/projeto/catalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projeto.catalog.entities.Category;
+import com.projeto.catalog.dto.CategoryDTO;
 import com.projeto.catalog.services.CategoryService;
 
 @RestController
@@ -19,9 +19,9 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
+	public ResponseEntity<List<CategoryDTO>> findAll(){
 		
-		List<Category> list = service.findAll();
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
--- backend/src/main/java/com/projeto/catalog/services/CategoryService.java
+++ backend/src/main/java/com/projeto/catalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.projeto.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.projeto.catalog.dto.CategoryDTO;
 import com.projeto.catalog.entities.Category;
 import com.projeto.catalog.repositories.CategoryRepository;
 
@@ -16,7 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 }
