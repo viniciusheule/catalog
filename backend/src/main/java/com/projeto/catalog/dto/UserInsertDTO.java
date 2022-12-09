25af41d65b522530bbbcb795a53a32c5e379b6ce
Vinicius Heule <viniciusheule@gmail.com>
2022-12-09 10:01:18 -0300

    Custom constraintValidator

--- backend/src/main/java/com/projeto/catalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/projeto/catalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.projeto.catalog.dto;
 
+import com.projeto.catalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
 	private static final long serialVersionUID = 1L;
--- backend/src/main/java/com/projeto/catalog/repositories/UserRepository.java
+++ backend/src/main/java/com/projeto/catalog/repositories/UserRepository.java
@@ -7,5 +7,7 @@ import com.projeto.catalog.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByEmail(String email);
 
 }
